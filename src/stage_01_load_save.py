8afe841d61657cfc946d01d642dc442bcec77a48
Lalit Aggarwal <agg.lalit@gmail.com>
2021-10-09 21:14:51 +0530

    Files updated

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -6,27 +6,23 @@ import pandas as pd
 from tqdm import tqdm
 import os
 
-def copy_file(src, dest):
-    list_of_files = os.listdir(src)
+def copy_file(source_download_dir, local_data_dir):
+    list_of_files = os.listdir(source_download_dir)
     N = len(list_of_files)
-    for file in tqdm(list_of_files, total = N, desc= f'Copying file for {src} to {dest}', color = 'green' ):
-        src_file = os.path.join(src, file)
-        dest_file = os.path.join(dest, file)
-        shutil.copy(src_file, dest_file)
+    for file in tqdm(list_of_files, total=N, desc=f'copying file from {source_download_dir} to {local_data_dir}', colour="green"):
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
+        shutil.copy(src, dest)
 
 def get_data(config_path):
     config = read_yaml(config_path)
 
-    source_download_dirs = config['source_download_dirs']
-    local_data_dirs = config['local_data_dirs']
+    source_download_dirs = config["source_download_dirs"]
+    local_data_dirs = config["local_data_dirs"]
 
-    create_directory([local_data_dirs])
-    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs), total = 2, desc = "List of folder", color = 'orange'):
-        copy_file(source_download_dir,local_data_dir)
-
-
-
-
+    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc= "list of folders", colour="red"):
+        create_directory([local_data_dir])
+        copy_file(source_download_dir, local_data_dir)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -35,4 +31,4 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    get_data(config_path="config/config.yaml")
+    get_data(config_path=parsed_args.config)
