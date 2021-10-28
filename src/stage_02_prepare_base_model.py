89485c21dfbf61c521eb280b3fae7c6ba5847b63
Lalit Aggarwal <agg.lalit@gmail.com>
2021-10-28 07:47:12 +0530

    stage 02 final update

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,13 +1,15 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model,prepare_model
 import argparse
-import shutil
+import io
 import logging
-import pandas as pd
-from tqdm import tqdm
 import os
 
-
+logging_str = "[%(asctime)s : %(levelname)s : %(module)s] : %(message)s"
+log_dirs = "logs"
+os.makedirs(log_dirs, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dirs, "running_logs.log"), 
+level= logging.INFO,format= logging_str, filemode= 'a' )
 
 def prepare_base_model(config_path,params_path):
     config = read_yaml(config_path)
@@ -26,6 +28,26 @@ def prepare_base_model(config_path,params_path):
 
     model = get_VGG_16_model(input_shape =params["IMAGE_SIZE"], model_path = base_model_path)
 
+    full_model = prepare_model(
+        model, 
+        CLASSES =params['CLASSES'],
+        freeze_all = True,
+        freeze_till = None,
+        learning_rate = params['LEARNING_RATE']
+    )
+
+    def _log_model_Summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn = lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+
+        return summary_str
+
+    logging.info(f"Full model summary :\n {_log_model_Summary(full_model)}")
+
+    updated_base_model_path = os.path.join(base_model_dir_path,updated_base_model_name)
+    full_model.save(updated_base_model_path)
+    
 
 
 
@@ -33,7 +55,7 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
-    args.add_argument("--params", "-c", default="params.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
 
     parsed_args = args.parse_args()
 
