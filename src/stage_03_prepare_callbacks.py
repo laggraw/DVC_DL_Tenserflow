263bf11d51cc3b44bcf1668e42ae48701af840cb
Lalit Aggarwal <agg.lalit@gmail.com>
2021-10-28 09:06:10 +0530

    stage 03 callback file is ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,5 +1,6 @@
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.models import get_VGG_16_model,prepare_model
+from src.utils.callbacks import create_and_save_tensorboard_callbacks,create_and_save_checkpoints_callbacks
 import argparse
 import io
 import logging
@@ -16,7 +17,17 @@ def prepare_callbacks(config_path,params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
+    artifacts = config['artifacts']
+    artifacts_dir = artifacts['ARTIFACTS_DIR']
 
+    tensorboard_log_dir = os.path.join(artifacts_dir, artifacts['TENSORBOARD_ROOT_LOG_DIR'])
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts['CHECKPOINT_DIR'])
+    callbacks_dir = os.path.join(artifacts_dir, artifacts['CALLBACKS_DIR'])
+
+    create_directory([tensorboard_log_dir,checkpoint_dir,callbacks_dir])
+
+    create_and_save_tensorboard_callbacks(callbacks_dir,tensorboard_log_dir)
+    create_and_save_checkpoints_callbacks(callbacks_dir,checkpoint_dir)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
